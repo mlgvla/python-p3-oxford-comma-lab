44d49076bd343137b47c094018034dc2763adc82
Monica Gerard <mlgvla@gmail.com>
2022-10-16 00:40:31 -0400

    solution

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    last_element = items[-1]
+    modified_list = items[0:-1]
+
+    if len(items) == 1:
+        return "".join(items)
+    elif len(items) == 2:
+        return ", ".join(modified_list) + " and " + last_element
+    else:
+        return ", ".join(modified_list) + ", and " + last_element
